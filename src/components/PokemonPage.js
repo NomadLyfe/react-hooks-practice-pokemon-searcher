057901cf6a0c69f39120394776d3094deeb203e7
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-06-09 16:56:53 -0400

    complete lab

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemonCollection, setPokemonCollection] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then(r => r.json())
+    .then(data => setPokemonCollection(data));
+  }, [])
+
+  if (!pokemonCollection) return <p>Loading...</p>
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm pokemonCollection={pokemonCollection} setPokemonCollection={setPokemonCollection} />
       <br />
-      <Search />
+      <Search pokemonCollection={pokemonCollection} setPokemonCollection={setPokemonCollection} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemonCollection={pokemonCollection} setPokemonCollection={setPokemonCollection} />
     </Container>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search() {
+function Search({ pokemonCollection, setPokemonCollection }) {
+  function handleChange(e) {
+    fetch("http://localhost:3001/pokemon")
+    .then(r => r.json())
+    .then(data => {
+      const updatedPokemon = data.filter(pokemon => {
+        if (e.target.value === "") {
+          return pokemon;
+        } else {
+          return pokemon.name.toLowerCase().includes(e.target.value.toLowerCase());
+        }
+      });
+      setPokemonCollection([...updatedPokemon]);
+    });
+  }
   return (
     <div className="ui search">
-      <div className="ui icon input">
-        <input className="prompt" />
+      <div className="ui icon input" >
+        <input className="prompt" onChange={handleChange} />
         <i className="search icon" />
       </div>
     </div>
